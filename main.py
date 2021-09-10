456dc6a6609440ed6acbea474cb7a4ca3ccc9ccf
JeG99 <a00824494@itesm.mx>
2021-09-10 16:05:49 -0500

    cities balancing is weird

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from my_maths import *
 from matplotlib import pyplot as plt
+import itertools
 
 def run_experiment():
-    graph = graph_gen(6)
-    path = annealing(0.3, 100, 0.9, graph, 500, 1.1)
-    x_seq = [city[0] for city in path]
-    x_seq.append(path[0][0])
-    y_seq = [city[1] for city in path]
-    y_seq.append(path[0][1])
-    plt.scatter(x_seq, y_seq)
-    plt.plot(x_seq, y_seq)
-    plt.show()
+    graph = graph_gen(12)
+    path = k_annealing(2, 0.1, 10, 0.9, graph, 100, 1.1)
+    
+    #x_seq = [city[0] for city in path]
+    #x_seq.append(path[0][0])
+    #y_seq = [city[1] for city in path]
+    #y_seq.append(path[0][1])
+    #plt.scatter(x_seq, y_seq)
+    #plt.plot(x_seq, y_seq)
+    #plt.show()
+
+    paths = []
+    result = [list(v) for k,v in itertools.groupby(path, key= lambda x: x==-1) if not k]
+
+    for j in result:
+        x_seq = [city[0] for city in j]
+        x_seq.append(j[0][0])
+        y_seq = [city[1] for city in j]
+        y_seq.append(j[0][1])
+        plt.scatter(x_seq, y_seq)
+        plt.plot(x_seq, y_seq)
+        plt.show()
 
 run_experiment()
--- my_maths.py
+++ my_maths.py
@@ -14,18 +14,84 @@ def temp_init(T_k, L_0, R_min, graph):
         T_k *= beta
     return T_k
 
+def k_temp_init(K, T_k, L_0, R_min, graph):
+    u = k_rand_paths(graph, K)
+    R_a = 0
+    beta = 1.5
+    while R_a < R_min:
+        u, R_a = markov(L_0, u, graph, T_k)
+        T_k *= beta
+    return T_k
+
+def k_rand_paths(graph, k):
+    temp_graph = graph.copy()
+    np.random.shuffle(temp_graph)
+    sublen = int(len(temp_graph) / k)
+    if sublen < 3:
+        raise Exception("Error: al menos 2 ciudades por viajero.")
+    for i in range(k - 1):
+        temp_graph.insert((sublen * (i + 1)), -1)
+    return temp_graph
+
+def k_mutate(path):
+    temp_path = path.copy()
+    index = np.random.randint(0, len(temp_path) - 2)
+    temp = temp_path[index]
+    temp_path[index] = temp_path[index + 1]
+    temp_path[index + 1] = temp
+    needs_correction, cities_per_traveler = count_cities(temp_path)
+
+    if needs_correction:
+        print(temp_path)
+        correct_paths(temp_path, cities_per_traveler)
+
+    return temp_path
+
+def count_cities(path):
+    cities = 0
+    index = 0
+    cities_per_traveler = []
+    needs_correction = False
+    for city in path:
+        if city != -1:
+            cities += 1
+        else:
+            cities_per_traveler.append((index, cities))
+            if cities < 2:
+                needs_correction = True
+            cities = 0
+        index += 1
+
+    return needs_correction, cities_per_traveler
+
+def correct_paths(paths, cities_per_traveler):
+    
+    needs_correction = True
+    while(needs_correction):
+        max_cities = max(cities_per_traveler, key=lambda x: x[1])
+        min_cities = min(cities_per_traveler, key=lambda x: x[1])
+        
+        #print(max_cities, min_cities)
+        #print(paths)
+
+        max_index = max_cities[0]
+        min_index = min_cities[0]
+        
+        gift = paths.pop(max_index - 1)
+        paths.insert(min_index - 1, gift)
+
+        needs_correction, cities_per_traveler = count_cities(paths)
+
 def markov(L_k, u, graph, T_k):
     j = 0
     for l in range(L_k):
-        v = mutate(u)
-        E_u = cost(graph, u)
-        E_v = cost(graph, v)
+        v = k_mutate(u)
+        E_u = sum(k_cost(graph, u))
+        E_v = sum(k_cost(graph, v))
         if E_v <= E_u:
-            #print('Ev < Eu')
             u = v.copy()
             j += 1
         elif np.random.uniform(0.0, 1.0) < boltzmann(E_u, E_v, T_k):
-            #print('metropolis', boltzmann(E_u, E_v, T_k))
             u = v.copy()
             j += 1
 
@@ -55,6 +121,19 @@ def cost(graph, path):
         total += euclid_dist(path[i-1], path[i])
     total += euclid_dist(path[-1], path[0])
     return total
+
+def k_cost(graph, path):
+    first_city = 0
+    total = 0
+    cost_list = []
+    for i in range(1, len(path)):
+        if path[i] != -1:
+            total += euclid_dist(path[i-1], path[i])
+        else:
+            total += euclid_dist(path[-1], path[first_city])
+            first_city = i + 1
+            total = 0
+    return cost_list
 
 def rand_path(graph):
     temp_graph = graph.copy()
@@ -90,6 +169,28 @@ def annealing(T_0, L_k, R_min, graph, iter, alpha):
             energy.append(cost(graph, u))
         #print('Eu =', cost(graph, u),'\t\tTk =', T_k, '\t\tRa =', R_a)
     print('Eu =', cost(graph, u),'\t\tTk =', T_k, '\t\tRa =', R_a)
+    plt.plot(energy)
+    plt.show()
+    return best_paths[-1]
+
+def k_annealing(K, T_0, L_k, R_min, graph, iter, alpha):
+    energy = []
+    best_paths = []
+    T_k = k_temp_init(K, T_0, L_k, R_min, graph)
+    print('T0 =', T_k)
+    k = 0
+    u = k_rand_paths(graph, K)
+    energy.append(sum(k_cost(graph, u)))
+    best_paths.append(u)
+    while(k < iter):
+        u, R_a = markov(L_k, u, graph, T_k)
+        k += 1
+        T_k *= alpha
+        if sum(k_cost(graph, u)) < sum(k_cost(graph, best_paths[-1])):
+            best_paths.append(u)
+            energy.append(sum(k_cost(graph, u)))
+        #print('Eu =', cost(graph, u),'\t\tTk =', T_k, '\t\tRa =', R_a)
+    #print('Eu =', sum(k_cost(graph, u)),'\t\tTk =', T_k, '\t\tRa =', R_a)
     plt.plot(energy)
     plt.show()
     return best_paths[-1]
